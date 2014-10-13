c98f84bba66399e430bb4da85cc4f85edb96c2e9
bingoogolapple <bingoogolapple@gmail.com>
2014-10-13 17:35:51 +0800

    添加GAEIndicator和ViewPager宽度不相同案例

--- demo/src/main/java/cn/bingoogolapple/loon/demo/ui/fragment/IndicatorFragment.java
+++ demo/src/main/java/cn/bingoogolapple/loon/demo/ui/fragment/IndicatorFragment.java
@@ -19,18 +19,25 @@ import cn.bingoogolapple.loon.library.LoonView;
  */
 @LoonLayout(id = R.layout.fragment_indicator)
 public class IndicatorFragment extends BaseFragment {
-    @LoonView(id = R.id.indicator)
-    private BGAIndicator mIndicator;
-    @LoonView(id = R.id.pager)
-    private ViewPager mPager;
-    private List<BGAIndicator.TabInfo> mTabInfos;
+    @LoonView(id = R.id.indicator1)
+    private BGAIndicator mIndicator1;
+    @LoonView(id = R.id.pager1)
+    private ViewPager mPager1;
+
+    @LoonView(id = R.id.indicator2)
+    private BGAIndicator mIndicator2;
+    @LoonView(id = R.id.pager2)
+    private ViewPager mPager2;
 
     @Override
     protected void processLogic(Bundle savedInstanceState) {
-        mTabInfos = new ArrayList<BGAIndicator.TabInfo>();
+        List<BGAIndicator.TabInfo> mTabInfos = new ArrayList<BGAIndicator.TabInfo>();
         mTabInfos.add(new BGAIndicator.TabInfo("选项一",NestFragment.NextOneFragment.class.getName()));
         mTabInfos.add(new BGAIndicator.TabInfo("选项二",NestFragment.NextTwoFragment.class.getName()));
         mTabInfos.add(new BGAIndicator.TabInfo("选项三",NestFragment.NextThreeFragment.class.getName()));
-        mIndicator.initData(3,mTabInfos,mPager,getChildFragmentManager());
+        mIndicator1.initData(0,mTabInfos,mPager1,getChildFragmentManager());
+        mIndicator2.initData(1,mTabInfos,mPager2,getChildFragmentManager());
+
+
     }
 }
